b0b6152f6211d206d0cc9569a07a8baf66d0bc37
Matt K <mattcs321@gmail.com>
2020-05-09 14:09:58 -0400

    added route to error page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import "./App.css"
 
+import {Route, Switch} from "react-router-dom"
+
 import Error from './pages/Error'
 //import logo from './logo.svg';
 //import './App.css';
 import Home from './pages/Home';
 import React from "react";
 import Rooms from './pages/Rooms';
-import {Route} from "react-router-dom"
 import SingleRoom from './pages/SingleRoom'
 
 function App() {
   return (
     <>
+    <Switch>
     <Route exact path="/" component={Home}/>
     <Route exact path="/rooms" component={Rooms}/>
     <Route exact path="/rooms/:slug" component={SingleRoom}/>
-    
-    
+    <Route component ={Error} />
+    </Switch>
     </>
     
   
