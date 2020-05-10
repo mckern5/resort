343bd4e3de4bbe290327d7390ca3166dbd4e1f97
Matt K <mattcs321@gmail.com>
2020-05-09 20:53:29 -0400

    add hero to error page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,9 +1,6 @@
+import Hero from '../components/Hero'
 import React from 'react'
 
 export default function Error() {
-    return (
-        <div>
-            hello from error page
-        </div>
-    )
+    return <Hero/>
 }
