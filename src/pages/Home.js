4fbed75883533d036bacca693c58b47456daf63e
Matt K <mattcs321@gmail.com>
2020-05-09 21:13:09 -0400

    add Banner to Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,19 @@
+import Banner from '../components/Banner'
 import Hero from  '../components/Hero'
+import {Link} from 'react-router-dom'
 import React from 'react'
 
 export default function Home() {
     return ( 
-    <Hero hero="defaultHero"/> 
-        
+    <Hero> 
+        <Banner title ="luxurious rooms"
+                 subtitle ="deluxe rooms starting at $299"
+        >
+           <Link to ='/rooms'  className="btn-primary">
+               our rooms
+           </Link>
+        </Banner>
+    </Hero>        
     
     )
     
